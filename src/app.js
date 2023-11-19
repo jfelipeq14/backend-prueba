705f32a308206432b237d765ec783391d0091311
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-19 11:31:47 -0500

    ruta de customers

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ const express = require("express")
 
 const config = require("./config")
 
+
+const customers = require("./modules/customers/routes")
 const app = express()
 
+// config
 app.set("port", config.app.port)
+
+//routes
+app.use("/api/customers", customers)
 
 module.exports = app
